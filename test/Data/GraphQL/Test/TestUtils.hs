fc35f15d98c6d377059c47b8e42666efa9f484b2
Brandon Chinn <brandon@leapyear.io>
2020-01-04 23:40:36 -0800

    Add tests for MockQueryT

---
+++ test/Data/GraphQL/Test/TestUtils.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Data.GraphQL.Test.TestUtils where
+
+import Control.Exception (SomeException, try)
+import Data.Aeson (object, (.=))
+import Data.Aeson.Schema (get)
+import Data.Either (isLeft)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@?), (@?=))
+
+import Data.GraphQL.Test.TestQuery (TestArgs(..), testQuery)
+import Data.GraphQL.Monad (runQuery)
+import Data.GraphQL.TestUtils (ResultMock(..), mocked, runMockQueryT)
+
+testTestUtils :: TestTree
+testTestUtils = testGroup "TestUtils"
+  [ testCase "MockQueryT returns a mock" $ do
+      obj <- runMockQueryT runTestQuery
+        [ mocked ResultMock
+            { query = testQuery
+            , args = TestArgs
+            , result = object [ "getUser" .= object [ "id" .= (20 :: Int) ] ]
+            }
+        ]
+      [get| obj.getUser.id |] @?= 20
+  , testCase "MockQueryT errors with no matching mock" $ do
+      obj <- try @SomeException $ runMockQueryT runTestQuery []
+      isLeft obj @? "MockQueryT did not error"
+  , testCase "MockQueryT removes mock after use" $ do
+      obj <- try @SomeException $ runMockQueryT (runTestQuery >> runTestQuery)
+        [ mocked ResultMock
+            { query = testQuery
+            , args = TestArgs
+            , result = object [ "getUser" .= object [ "id" .= (20 :: Int) ] ]
+            }
+        ]
+      isLeft obj @? "MockQueryT did not error"
+  ]
+  where
+    runTestQuery = runQuery testQuery TestArgs
--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,10 @@
 import Test.Tasty (defaultMain, testGroup)
 
 import Data.GraphQL.Test.Monad.Class
+import Data.GraphQL.Test.TestUtils
 
 main :: IO ()
 main = defaultMain $ testGroup "graphql-client"
   [ testRunQuery
+  , testTestUtils
   ]
