193ad6ffee5a2ac82c6fac0b5a94d5a76210fbb6
Brandon Chinn <brandon@leapyear.io>
2020-01-22 13:05:46 -0800

    Test parseVariableDefinitions

---
+++ graphql-codegen-haskell/src/parse/variableDefinition.test.ts
@@ -0,0 +1,136 @@
+import gql from 'graphql-tag'
+
+import { parseVariableDefinitions } from './variableDefinition'
+
+it('parses variable definitions', () => {
+  const query = gql`
+    query(
+      $int: Int
+      $float: Float
+      $string: String
+      $bool: Boolean
+      $id: ID
+      $scalar: MyScalar
+      $nonNull: String!
+      $list: [Int]
+      $listList: [[Int]]
+      $nonNullList: [Int]!
+      $nonNullListNonNull: [Int!]!
+    ) {
+      id
+    }
+  `
+
+  expect(
+    parseVariableDefinitions(query.definitions[0].variableDefinitions)
+  ).toStrictEqual([
+    {
+      name: 'int',
+      type: {
+        list: false,
+        name: 'Int',
+        nullable: true,
+      },
+    },
+    {
+      name: 'float',
+      type: {
+        list: false,
+        name: 'Float',
+        nullable: true,
+      },
+    },
+    {
+      name: 'string',
+      type: {
+        list: false,
+        name: 'String',
+        nullable: true,
+      },
+    },
+    {
+      name: 'bool',
+      type: {
+        list: false,
+        name: 'Boolean',
+        nullable: true,
+      },
+    },
+    {
+      name: 'id',
+      type: {
+        list: false,
+        name: 'ID',
+        nullable: true,
+      },
+    },
+    {
+      name: 'scalar',
+      type: {
+        list: false,
+        name: 'MyScalar',
+        nullable: true,
+      },
+    },
+    {
+      name: 'nonNull',
+      type: {
+        list: false,
+        name: 'String',
+        nullable: false,
+      },
+    },
+    {
+      name: 'list',
+      type: {
+        list: true,
+        inner: {
+          list: false,
+          name: 'Int',
+          nullable: true,
+        },
+        nullable: true,
+      },
+    },
+    {
+      name: 'listList',
+      type: {
+        list: true,
+        inner: {
+          list: true,
+          inner: {
+            list: false,
+            name: 'Int',
+            nullable: true,
+          },
+          nullable: true,
+        },
+        nullable: true,
+      },
+    },
+    {
+      name: 'nonNullList',
+      type: {
+        list: true,
+        inner: {
+          list: false,
+          name: 'Int',
+          nullable: true,
+        },
+        nullable: false,
+      },
+    },
+    {
+      name: 'nonNullListNonNull',
+      type: {
+        list: true,
+        inner: {
+          list: false,
+          name: 'Int',
+          nullable: false,
+        },
+        nullable: false,
+      },
+    },
+  ])
+})
