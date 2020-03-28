526a23271cb409d37f8ba585257dd3eb23a3694e
Brandon Chinn <brandon@leapyear.io>
2020-03-27 18:05:21 -0700

    Revert "Also generate parent enum module and re-export in api module"
    This reverts commit 0cef2bd45c5bd74a3f70e122a5364a9e01784ef2.

--- graphql-codegen-haskell/src/index.ts
+++ graphql-codegen-haskell/src/index.ts
@@ -8,7 +8,7 @@ import { RawPluginConfig, resolveConfig, validateConfig } from './config'
 import { parseFragments } from './parse/fragments'
 import { parseOperations } from './parse/operation'
 import { renderAPIModule } from './render/api'
-import { renderEnumModule, renderEnumParentModule } from './render/enum'
+import { renderEnumModule } from './render/enum'
 import { moduleToPath, writeFile } from './utils'
 
 export const plugin: PluginFunction<RawPluginConfig> = (
@@ -25,31 +25,17 @@ export const plugin: PluginFunction<RawPluginConfig> = (
 
   const parsedFragments = parseFragments(ast)
   const { enums, operations } = parseOperations(ast, schema, parsedFragments)
-  const enumModules = [] as string[]
 
-  if (enums.length > 0) {
-    // Generate the module for each enum
-    enums.forEach((parsedEnum) => {
-      const { enumModuleName, enumModule } = renderEnumModule(
-        config,
-        parsedEnum
-      )
+  const renderedEnums = enums.map((parsedEnum) =>
+    renderEnumModule(config, parsedEnum)
+  )
 
-      const enumModulePath = moduleToPath(enumModuleName, config.hsSrcDir)
-      writeFile(enumModulePath, enumModule)
+  renderedEnums.forEach(({ enumModuleName, enumModule }) => {
+    const enumModulePath = moduleToPath(enumModuleName, config.hsSrcDir)
+    writeFile(enumModulePath, enumModule)
+  })
 
-      enumModules.push(enumModuleName)
-    })
-
-    // Generate the parent enum module
-    const enumParentModulePath = moduleToPath(
-      config.enumsModule,
-      config.hsSrcDir
-    )
-    const enumParentModule = renderEnumParentModule(config, enumModules)
-    writeFile(enumParentModulePath, enumParentModule)
-  }
-
+  const enumModules = renderedEnums.map(({ enumModuleName }) => enumModuleName)
   return renderAPIModule(config, enumModules, operations)
 }
 
