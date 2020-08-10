0d64fde3bcb0a8424ba149f1e6c9e0e83d909b55
Brandon Chinn <brandon@leapyear.io>
2020-08-10 13:32:31 -0700

    Use MonadTrans trick

--- graphql-client/src/Data/GraphQL/Monad/Class.hs
+++ graphql-client/src/Data/GraphQL/Monad/Class.hs
@@ -8,11 +8,13 @@ Defines the 'MonadQuery' type class, which allows GraphQL
 queries to be run and mocked.
 -}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Data.GraphQL.Monad.Class
   ( MonadQuery(..)
@@ -21,17 +23,7 @@ module Data.GraphQL.Monad.Class
 
 import Control.Exception (throwIO)
 import Control.Monad.IO.Class (MonadIO(..))
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Except (ExceptT)
-import Control.Monad.Trans.Identity (IdentityT)
-import Control.Monad.Trans.Maybe (MaybeT)
-import Control.Monad.Trans.Reader (ReaderT)
-import qualified Control.Monad.Trans.RWS.Lazy as Lazy
-import qualified Control.Monad.Trans.RWS.Strict as Strict
-import qualified Control.Monad.Trans.State.Lazy as Lazy
-import qualified Control.Monad.Trans.State.Strict as Strict
-import qualified Control.Monad.Trans.Writer.Lazy as Lazy
-import qualified Control.Monad.Trans.Writer.Strict as Strict
+import Control.Monad.Trans.Class (MonadTrans(..))
 import Data.Aeson.Schema (IsSchemaObject, Object, SchemaType)
 import Data.Maybe (fromJust)
 
@@ -59,32 +51,5 @@ runQuery query = do
 
 {- Instances for common monad transformers -}
 
-instance MonadQuery m => MonadQuery (ReaderT r m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance MonadQuery m => MonadQuery (ExceptT e m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance MonadQuery m => MonadQuery (IdentityT m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance MonadQuery m => MonadQuery (MaybeT m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance (Monoid w, MonadQuery m) => MonadQuery (Lazy.RWST r w s m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance (Monoid w, MonadQuery m) => MonadQuery (Strict.RWST r w s m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance MonadQuery m => MonadQuery (Lazy.StateT s m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance MonadQuery m => MonadQuery (Strict.StateT s m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance (Monoid w, MonadQuery m) => MonadQuery (Lazy.WriterT w m) where
-  runQuerySafe = lift . runQuerySafe
-
-instance (Monoid w, MonadQuery m) => MonadQuery (Strict.WriterT w m) where
+instance {-# OVERLAPPABLE #-} (Monad (t m), MonadTrans t, MonadQuery m) => MonadQuery (t m) where
   runQuerySafe = lift . runQuerySafe
