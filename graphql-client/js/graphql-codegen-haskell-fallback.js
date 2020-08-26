05ae2fa367dbd09bef4f8448fb288b671a055a6c
Brandon Chinn <brandon@leapyear.io>
2020-08-26 12:51:15 -0700

    Copy GRAPHQL_CODEGEN file into fallback directory

--- graphql-client/js/graphql-codegen-haskell-fallback.js
+++ graphql-client/js/graphql-codegen-haskell-fallback.js
@@ -3,11 +3,12 @@
  * isn't built yet.
  *
  * If this is the case, building `graphql-client` SHOULD NOT fail, but running
- * `graphql-codegen` SHOULD fail UNLESS the user builds
+ * `graphql-codegen` SHOULD fail UNLESS the user gets a copy of
  * `graphql-codegen-haskell.js` themselves and points to it via the
  * `GRAPHQL_CODEGEN` environment variable.
  */
 
+const fs = require('fs')
 const path = require('path')
 const utils = require('__utils')
 
@@ -36,18 +37,27 @@ const error = (msg) => {
   process.exit(1)
 }
 
+const requireFile = (file) => {
+  try {
+    return require(file)
+  } catch (e) {
+    error(`Could not load file ${file}:\n${e}`)
+  }
+}
+
 const GRAPHQL_CODEGEN = process.env.GRAPHQL_CODEGEN
 if (GRAPHQL_CODEGEN === undefined) {
   error('GRAPHQL_CODEGEN environment variable not set')
 }
 
-const codegenFile = path.resolve(process.cwd(), GRAPHQL_CODEGEN)
-let codegen
+const codegenFileSrc = path.resolve(process.cwd(), GRAPHQL_CODEGEN)
+const codegenFile = path.resolve(
+  __dirname,
+  './graphql-codegen-haskell-external.js'
+)
 
-try {
-  codegen = require(codegenFile)
-} catch (e) {
-  error(`Could not load file ${codegenFile}:\n${e}`)
-}
+// require(...) searches for a node_modules directory in the parents of the JS
+// file being required, so copy the file into here.
+fs.copyFileSync(codegenFileSrc, codegenFile)
 
-codegen.main()
+requireFile(codegenFile).main()
