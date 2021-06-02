e47fd6eb7ccd04894ec1d02489d57fece4f95d8c
Brandon Chinn <brandon@leapyear.io>
2021-06-01 21:09:16 -0700

    Expose GraphQLManager + runQuerySafeIO

--- graphql-client/src/Data/GraphQL/Monad.hs
+++ graphql-client/src/Data/GraphQL/Monad.hs
@@ -4,21 +4,35 @@ Maintainer  :  Brandon Chinn <brandon@leapyear.io>
 Stability   :  experimental
 Portability :  portable
 
-Defines the 'GraphQLQueryT' monad transformer, which implements
-'MonadGraphQLQuery' to allow querying GraphQL APIs.
+Defines the 'MonadGraphQLQuery' type class to query GraphQL APIs.
+Also provides the 'GraphQLQueryT' monad transformer that can be
+added to a transformer stack to implement the type class, and the
+'runQuerySafeIO' function to manually implement it yourself.
 -}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Data.GraphQL.Monad
-  ( module Data.GraphQL.Monad.Class
-  , GraphQLQueryT
-  , runGraphQLQueryT
+  ( -- * MonadGraphQLQuery API
+    MonadGraphQLQuery(..)
+  , runQuery
+  , runQuerySafeIO
+
+    -- * GraphQLSettings
   , GraphQLSettings(..)
   , defaultGraphQLSettings
+
+    -- * GraphQLManager
+  , GraphQLManager
+  , initGraphQLManager
+
+    -- * GraphQLQueryT
+  , GraphQLQueryT
+  , runGraphQLQueryT
   ) where
 
 import Control.Monad.IO.Class (MonadIO(..))
@@ -27,6 +41,7 @@ import Control.Monad.Reader (ReaderT, ask, runReaderT)
 import Control.Monad.Trans.Class (MonadTrans)
 import Data.Aeson ((.=))
 import qualified Data.Aeson as Aeson
+import Data.Aeson.Schema (Object)
 import Network.HTTP.Client
     ( Manager
     , ManagerSettings
@@ -42,10 +57,11 @@ import Network.HTTP.Types (hContentType)
 
 import Data.GraphQL.Monad.Class
 import Data.GraphQL.Query (GraphQLQuery(..))
+import Data.GraphQL.Result (GraphQLResult)
 
 {- GraphQLSettings -}
 
--- | The settings for running GraphQLQueryT.
+-- | The settings for initializing a 'GraphQLManager'.
 data GraphQLSettings = GraphQLSettings
   { managerSettings :: ManagerSettings
     -- ^ Uses TLS by default
@@ -53,7 +69,17 @@ data GraphQLSettings = GraphQLSettings
   , modifyReq       :: Request -> Request
   }
 
--- | Default query settings.
+-- | Default settings for 'GraphQLSettings'. Requires 'url' field to be overridden.
+--
+-- Example usage:
+--
+-- >>> defaultGraphQLSettings
+-- ...   { url = "https://api.github.com/graphql"
+-- ...   , modifyReq = \\req -> req
+-- ...       { requestHeaders =
+-- ...           (hAuthorization, "bearer my_github_token") : requestHeaders req
+-- ...       }
+-- ...   }
 defaultGraphQLSettings :: GraphQLSettings
 defaultGraphQLSettings = GraphQLSettings
   { managerSettings = tlsManagerSettings
@@ -61,13 +87,44 @@ defaultGraphQLSettings = GraphQLSettings
   , modifyReq = id
   }
 
--- | The state for running GraphQLQueryT.
+{- The base runQuerySafeIO implementation -}
+
+-- | The manager for running GraphQL queries.
 data GraphQLManager = GraphQLManager
   { manager :: Manager
   , baseReq :: Request
   }
 
--- | The monad transformer type that should be used to run GraphQL queries.
+initGraphQLManager :: GraphQLSettings -> IO GraphQLManager
+initGraphQLManager GraphQLSettings{..} = do
+  manager <- newManager managerSettings
+  baseReq <- modifyReq . modifyReq' <$> parseUrlThrow url
+  return GraphQLManager{..}
+  where
+    modifyReq' req = req
+      { method = "POST"
+      , requestHeaders = (hContentType, "application/json") : requestHeaders req
+      }
+
+-- | Execute a GraphQL query with the given 'GraphQLManager'.
+runQuerySafeIO
+  :: (GraphQLQuery query, schema ~ ResultSchema query)
+  => GraphQLManager -> query -> IO (GraphQLResult (Object schema))
+runQuerySafeIO GraphQLManager{..} query = httpLbs request manager >>= decodeBody
+  where
+    request =
+      baseReq
+        { requestBody = RequestBodyLBS $ Aeson.encode $ Aeson.object
+            [ "query" .= getQueryText query
+            , "variables" .= getArgs query
+            ]
+        }
+
+    decodeBody = either fail return . Aeson.eitherDecode . responseBody
+
+{- GraphQLQueryT monad transformer -}
+
+-- | The monad transformer type that can be used to run GraphQL queries.
 --
 -- @
 -- newtype MyMonad a = MyMonad { unMyMonad :: GraphQLQueryT IO a }
@@ -75,13 +132,7 @@ data GraphQLManager = GraphQLManager
 -- runMyMonad :: MyMonad a -> IO a
 -- runMyMonad = runGraphQLQueryT graphQLSettings . unMyMonad
 --   where
---     graphQLSettings = defaultGraphQLSettings
---       { url = "https://api.github.com/graphql"
---       , modifyReq = \\req -> req
---           { requestHeaders =
---               (hAuthorization, "bearer my_github_token") : requestHeaders req
---           }
---       }
+--     graphQLSettings = defaultGraphQLSettings{url = "https://api.github.com/graphql"}
 -- @
 newtype GraphQLQueryT m a = GraphQLQueryT { unGraphQLQueryT :: ReaderT GraphQLManager m a }
   deriving
@@ -97,30 +148,11 @@ instance MonadUnliftIO m => MonadUnliftIO (GraphQLQueryT m) where
 
 instance MonadIO m => MonadGraphQLQuery (GraphQLQueryT m) where
   runQuerySafe query = do
-    GraphQLManager{..} <- GraphQLQueryT ask
+    manager <- GraphQLQueryT ask
+    liftIO $ runQuerySafeIO manager query
 
-    let request = baseReq
-          { requestBody = RequestBodyLBS $ Aeson.encode $ Aeson.object
-              [ "query" .= getQueryText query
-              , "variables" .= getArgs query
-              ]
-          }
-
-    liftIO $ either fail return . Aeson.eitherDecode . responseBody =<< httpLbs request manager
-
--- | Run a GraphQLQueryT stack.
+-- | Run the GraphQLQueryT monad transformer.
 runGraphQLQueryT :: MonadIO m => GraphQLSettings -> GraphQLQueryT m a -> m a
-runGraphQLQueryT GraphQLSettings{..} m = do
-  state <- liftIO $ do
-    manager <- newManager managerSettings
-    baseReq <- modifyReq . modifyReq' <$> parseUrlThrow url
-    return GraphQLManager{..}
-
-  (`runReaderT` state)
-    . unGraphQLQueryT
-    $ m
-  where
-    modifyReq' req = req
-      { method = "POST"
-      , requestHeaders = (hContentType, "application/json") : requestHeaders req
-      }
+runGraphQLQueryT settings m = do
+  manager <- liftIO $ initGraphQLManager settings
+  (`runReaderT` manager) . unGraphQLQueryT $ m
